3ffb9295e969ed558bed670da0dc42e9287f2575
mincheol <homerjay@naver.com>
2023-03-27 17:21:48 +0900

    settings, login Screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:tiktok_flutter/constants.dart';
+import 'package:tiktok_flutter/views/screens/auth/login_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,8 +13,14 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      home: const Text('Hello World')
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark().copyWith(
+        scaffoldBackgroundColor: backgroundColor
+      ),
+      title: 'TikTok Clone',
+      home: Scaffold(
+        body: LoginScreen(),
+      ),
     );
   }
 }
